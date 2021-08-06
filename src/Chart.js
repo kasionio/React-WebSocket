e811b425b21385946f62fd4fdbaf5b40fb9a2fbe
kasionio <kasianenko.illia@gmail.com>
2021-08-06 15:16:43 +0300

    rename some variables

--- src/Chart.js
+++ src/Chart.js
@@ -2,21 +2,21 @@ import React, { useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import styles from "./index.css";
 
-let dataX = [];
-let dataY = [];
-dataX.length = 5; //X-axis length
-dataY.length = dataX.length;
+let firstLineData = [];
+let secondLineData = [];
+firstLineData.length = 5; //X-axis length
+secondLineData.length = firstLineData.length;
 
 export function Chart({ data }) {
     useEffect(() => {
         if (data.dataFromServer) {
             if (data.dataFromServer[0].data <= 100) {
-                dataX.push(data.dataFromServer[0].data);
-                dataX.shift();
+                firstLineData.push(data.dataFromServer[0].data);
+                firstLineData.shift();
             }
             if (data.dataFromServer[1].data <= 100) {
-                dataY.push(data.dataFromServer[1].data);
-                dataY.shift();
+                secondLineData.push(data.dataFromServer[1].data);
+                secondLineData.shift();
             }
         }
     }, [data]);
@@ -29,17 +29,17 @@ export function Chart({ data }) {
                     animations: false,
                 }}
                 data={{
-                    labels: dataX,
+                    labels: firstLineData,
                     datasets: [
                         {
                             label: ["Data1"],
-                            data: dataX,
+                            data: firstLineData,
                             backgroundColor: ["lightgrey"],
                             borderColor: ["lightgrey"],
                         },
                         {
                             label: ["Data2"],
-                            data: dataY,
+                            data: secondLineData,
                             backgroundColor: ["#000"],
                             borderColor: ["#000"],
                         },
